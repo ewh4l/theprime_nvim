12e8f6fa2199eaed005b496b5703d7b9dd7b5821
Eric Whale <61775837+whal-e3@users.noreply.github.com>
2024-01-23 17:42:54 +0900

    Update set.lua

--- lua/ewhal/set.lua
+++ lua/ewhal/set.lua
@@ -22,7 +22,7 @@ vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
@@ -31,3 +31,6 @@ vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
 
 vim.o.mouse = ""
+
+vim.opt.binary = true
+vim.opt.eol = false
